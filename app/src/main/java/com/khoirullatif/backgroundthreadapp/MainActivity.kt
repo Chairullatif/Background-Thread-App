a6674f366d8b7c0410a3e23df61bfd2019e73b63
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-07-23 11:44:21 +0700

    menambahkan proses yang berjalan di thread lain

--- app/src/main/java/com/khoirullatif/backgroundthreadapp/MainActivity.kt
+++ app/src/main/java/com/khoirullatif/backgroundthreadapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.khoirullatif.backgroundthreadapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.khoirullatif.backgroundthreadapp.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,19 +16,45 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        // PROSES YANG BERJALAN DI BACKGROUND THREAD
+//        binding.btnStart.setOnClickListener {
+//            try {
+//                for (i in 1..10) {
+//                    Thread.sleep(500)
+//                    val percentage = i*10
+//                    if (percentage == 100) {
+//                        binding.tvStatus.text = getString(R.string.task_completed)
+//                    } else {
+//                        binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+//                    }
+//                }
+//            } catch (e: InterruptedException) {
+//                e.printStackTrace()
+//            }
+//        }
+
+
+        // PROSES YANG BERJALAN DI THREAD LAIN
         binding.btnStart.setOnClickListener {
-            try {
-                for (i in 1..10) {
-                    Thread.sleep(500)
-                    val percentage = i*10
-                    if (percentage == 100) {
-                        binding.tvStatus.text = getString(R.string.task_completed)
-                    } else {
-                        binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+            executor.execute {
+                try {
+                    for (i in 1..10) {
+                        Thread.sleep(1000)
+                        val percentage = i * 10
+                        handler.post {
+                            if (percentage == 100) {
+                                binding.tvStatus.text = getString(R.string.task_completed)
+                            } else {
+                                binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
                     }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
             }
         }
     }
